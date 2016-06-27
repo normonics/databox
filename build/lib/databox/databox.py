a56545ae449d983e79d0df684f94d005f5c1a51b
Joe Norman <joe.w.norman@gmail.com>
2016-06-27 17:31:18 -0400

    Added functions, cleaned things up

--- build/lib/databox/databox.py
+++ build/lib/databox/databox.py
@@ -11,13 +11,24 @@ import scipy.stats as stats
 
 #------------------------------------------------------------------------    
 
+def mongo_to_df(collection, query={}, fields=None):
+    """return a mongo query as a DataFrame
+    """
+
+    records = collection.find(query, projection=fields)
+    
+    return pd.DataFrame(list(records))
+
+#------------------------------------------------------------------------    
+
 def pca(df):
-    """Perform a principal components analysis on data in a DataFrame"""
+    """Perform a principal components analysis on data in a DataFrame
+    """
     
     # normalize the data
     df_normalized = (df - df.mean()) / df.std()
-    cov_matrix = np.cov(df_normalized, rowvar=0)
-    eig_values, eig_vectors = np.linalg.eig(cov_matrix)
+    corr_matrix = np.corrcoef(df_normalized, rowvar=0)
+    eig_values, eig_vectors = np.linalg.eig(corr_matrix)
     
     # sort eig values and eigvectors
     eig_vectors = np.array(
@@ -50,10 +61,12 @@ def plot_pca(projected_points, eig_values, eig_vectors, labels):
     ax[0,1].set_title('PCA scatter')
 
     ax[1,1].bar(np.arange(eig_vectors.shape[0]) + 0.1, eig_vectors[:,0])
+    ax[1,1].xaxis.set_ticks(np.arange(len(labels)) + 0.5)
     ax[1,1].xaxis.set_ticklabels(labels)
     ax[1,1].set_title('PC1')
 
     ax[0,0].bar(np.arange(eig_vectors.shape[0]) + 0.1, eig_vectors[:,1])
+    ax[0,0].xaxis.set_ticks(np.arange(len(labels)) + 0.5)
     ax[0,0].xaxis.set_ticklabels(labels)
     ax[0,0].set_title('PC2')
 
@@ -64,16 +77,33 @@ def plot_pca(projected_points, eig_values, eig_vectors, labels):
 
 #------------------------------------------------------------------------    
 
-def mongo_to_df(collection, query={}, fields=None):
-    """return a mongo query as a DataFrame"""
-    records = collection.find(query, projection=fields)
-    
-    return pd.DataFrame(list(records))
+
+
 
 #------------------------------------------------------------------------    
 
-def cross_scatter(df_1, df_2):
-    "Takes two DataFrames and builds the scatter plots of all their combinations"
+def mc_pca(df, N=100):
+    """Take a dataframe and establish the significance of the largest eigenvalue 
+    via a monte carlo approach
+    """
+
+    _, eig_values, _ = pca(df) 
+    true_largest_eig_value = eig_values[0]
+
+    largest_eig_values = np.zeros(N)
+    for n in xrange(N):
+        df_shuffled = shuffle_df(df)
+        _, eig_values, _ = pca(df_shuffled)
+        largest_eig_values[n] = eig_values[0]
+
+    return true_largest_eig_value, largest_eig_values
+
+
+#------------------------------------------------------------------------    
+
+def cross_scatter(df_1, df_2, lin_regress=True):
+    """Takes two DataFrames and builds the scatter plots of all their combinations
+    """
 
     labels_1 = df_1.columns
     labels_2 = df_2.columns
@@ -94,14 +124,27 @@ def cross_scatter(df_1, df_2):
             x = np.linspace(df_1[label_1].min(), df_1[label_1].max())
             ax[j,i].plot(x, slope*x+intercept, 'r', lw=3)
             
-            
             if i != 0:
                 ax[j,i].set_yticklabels([])
-            if j != df_2.shape[0]-1:
+            if j != df_2.shape[1]-1:
                 ax[j,i].set_xticklabels([])
             if i == 0:
                 ax[j,i].set_ylabel(label_2)
-            if j == df_2.shape[0]-1:
+            if j == df_2.shape[1]-1:
                 ax[j,i].set_xlabel(label_1)
                 for ticklabel in ax[j,i].get_xticklabels():
                     ticklabel.set_rotation('vertical')
+
+    return fig, ax
+
+#------------------------------------------------------------------------    
+
+def shuffle_df(df):
+    """Shuffles the values in each column of a DataFrame and returns the shuffled frame.
+    Useful for e.g. monte carlo analysis
+    """
+
+    df = df.copy()
+    df.apply(np.random.shuffle, axis=0)
+
+    return df 
